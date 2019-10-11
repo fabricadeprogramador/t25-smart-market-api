29f9fabd8af2d2cf0e1e826a9143b838ac4e6d36
Rodrigo Reis <rodrigoreis09@gmail.com>
2019-10-10 20:45:27 -0400

    resolvendo conflitos

--- model/Contato.js
+++ model/Contato.js
@@ -1,21 +1,15 @@
 class Contato {
 
-    constructor(id, cliente, assunto, mensagem, datacontato) {
+    constructor(id, cliente, assunto, mensagem, datacontato, icon, resposta, respondido) {
         this.id = id
         this.cliente = cliente
         this.assunto = assunto
         this.mensagem = mensagem
         this.datacontato = datacontato
-<<<<<<< HEAD
-
         this.icon = icon
-
-=======
-       
         this.resposta= ""
         this.respondido = false
         
->>>>>>> 8f36a11fdb5700b434411a0fda15f9a47533dcc2
     }
 }
 
