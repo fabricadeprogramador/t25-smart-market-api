efed43f1a3e6c8eee61cb9f75da779ba1f01bc97
Rodrigo Reis <rodrigoreis09@gmail.com>
2019-11-12 19:21:25 -0400

    Autenticação usuário

--- controller/UsuarioController.js
+++ controller/UsuarioController.js
@@ -54,20 +54,14 @@ class UsuarioController {
         }
     }
 
-    static async autenticar(req, res) {
-
-        
-        try {
-          
-            let objUsuario = req.body
-            
+    static async autenticar(req, res) {        
+        try {          
+            let objUsuario = req.body            
             res.json(await Usuario.find(objUsuario));
         } catch (error) {
             res.status(500).send(`Erro ao logar no sistema: ${error}`)
         }
     }
-
-
 }
 
 module.exports = UsuarioController;
